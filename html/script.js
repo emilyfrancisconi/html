56535c9f61cb972c1124064c1cb273f7e015ef2d
emilyfrancisconi <francisconi.emily@escola.pr.gov.br>
2025-05-22 12:15:51 -0300

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hambueger = document.getElementsById('hamburger');
+    const navMenu = document.getElementById('navMenu');
+});

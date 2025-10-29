d35101af867f098130335b08c6a94cecbf571bb3
emilyfrancisconi <francisconi.emily@escola.pr.gov.br>
2025-10-29 09:44:59 -0300

    Add files via upload

--- emily/script.js
+++ emily/script.js
@@ -1,3 +1,7 @@
 const titulo = document.createElement=('div');
 titulo.id='titulo';
 titulo.color = 'white';
+titulo.style.fontSize = '32px';
+titulo.style.fontFamily = 'Arial, sans-serif';
+titulo.style.textAlign = 'center';
+titulo.style.marginTop ='100px';
